e50c74ffe89ed1b97c86fbdfe8ee05ab80bf5750
Iwona Antonik <iwona.antonik@gmail.com>
2019-02-15 12:16:43 -0500

    first working version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,40 +21,36 @@ var trivia = [
 ]
 
 var questionIterator = 0;
-var questionGroup = trivia[questionIterator];
 var sec = 10;
 var timer;
-var correctlyAnswered;
-var incorrectlyAnswered;
+var correctlyAnswered=0;
+var incorrectlyAnswered=0;
+var unanswered = 0;
 
 $(document).ready(function () {
     $("#start").click(function () {
         document.getElementById("start").style.display = "none";
-        startTimer();
-        loadQuestion();
+            startTimer();
+            loadQuestion();            
         
-
     });
-
 })
 
-
 function loadQuestion() {
-    $("#question").html(questionGroup.question);
-    questionGroup.answers.forEach(element => {
+    $("#question").html(trivia[questionIterator].question);
+    trivia[questionIterator].answers.forEach(element => {
         $(`<p onclick="validateAnswer(this.id)" id="${element}">${element}</p>`).appendTo(`#answers`);
     });
-
-
 }
 
 function tick() {
     document.getElementById("timer").innerHTML = `Time Remaining: ${sec} Seconds`;;
     sec--;
     if (sec < 0) {
+        $( "#time-is-up" ).html(`<p> Time is up. <br> The answer answer is ${trivia[questionIterator].correctAnswer}.</p>`).show();
+        unanswered++;
         clearInterval(timer);
-        $("#answers").empty();
-        $(`<p> Time is up. <br> The answer answer is ${questionGroup.correctAnswer}.</p>`).appendTo("#answers")
+        setTimeout(next, 5000);
     }
 };
 
@@ -66,34 +62,37 @@ function startTimer() {
 function validateAnswer(ansId) {
     var userAnswer;
     userAnswer = ansId;
+    clearInterval(timer);
 
-    if (userAnswer === questionGroup.correctAnswer) {
-        $("#answers").empty();
-        $(`<p> Correct Answer.</p>`).appendTo("#answers")
-        correctlyAnswered++;
-        clearInterval(timer);
+    if (userAnswer === trivia[questionIterator].correctAnswer) {
+        $("#correct-answer").html(`<p> Correct Answer.</p>`).show();
+        correctlyAnswered++
+        setTimeout(next, 5000);
     }
     else {
-        $("#answers").empty();
-        $(`<p> Incorrect Answer.</p>`).appendTo("#answers")
+        $("#incorrect-answer").html(`<p> Incorrect Answer.</p>`).show();
         incorrectlyAnswered++;
-        clearInterval(timer);
+        setTimeout(next, 5000);
+    }
+}
+
+function next(){
+    questionIterator++
+    if (questionIterator<trivia.length){
+        $("#answers").empty();
+        document.getElementById("correct-answer").style.display = "none";
+        document.getElementById("incorrect-answer").style.display = "none";
+        document.getElementById("time-is-up").style.display = "none";
+        sec = 10;
+        startTimer();
+        loadQuestion();
+    }
+    else{
+
+        $("#game-over").html(`<p>Game Over!</p><br><p>Correct Answers: ${correctlyAnswered}</p><br><p>Incorrect Answers: ${incorrectlyAnswered}</p><br><p>Unanswered: ${unanswered}</p>`).show();
     }
 }
 
 
 
 
-// $(document).ready(function () {
-    // var timer = setInterval(function () {
-    //     document.getElementById("timer").innerHTML = `Time Remaining: ${timeleft}  Seconds`;
-    //     timeleft -= 1;
-    //     if (timeleft < 0) {
-    //         clearInterval(timer);
-    //         validateAnswer()
-    //         document.getElementById("timer").innerHTML = "Time's up"
-    //     }
-    // }, 1000);
-
-
-// })
